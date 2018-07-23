f7eb5d7664574e11f4d35fc4081aa4e40d641ee8
wx91 <766221790@qq.com>
2018-07-23 11:05:10 +0800

    使用curator框架操作zookeeper

---
+++ 互联网架构师第二期/Zookeeper/Zookeeper03/src/main/java/com/wangx/curator/cluster/Client2.java
@@ -0,0 +1,8 @@
+package com.wangx.curator.cluster;
+
+public class Client2 {
+	public static void main(String[] args) throws Exception {
+		CuratorWatcher watcher = new CuratorWatcher();
+		Thread.sleep(100000000);
+	}
+}
