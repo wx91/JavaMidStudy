b4f4a6dfa11e1415e8b03e15199840abaaf90174
wx91 <766221790@qq.com>
2018-09-04 09:59:39 +0800

    针对JVM参数设置

--- 互联网架构师第二期/JVM/JVMTest/src/main/java/com/wangx/base001/Test01.java
+++ 互联网架构师第二期/JVM/JVMTest/src/main/java/com/wangx/base001/Test01.java
@@ -2,7 +2,28 @@ package com.wangx.base001;
 
 public class Test01 {
 	public static void main(String[] args) {
+		// -XX:+UserCompressedOops -XX:UseLargePagesIndividuakAllocation
 
-  	}
+		// 1：
+		// -XX:+PrintGC -Xms5m -Xms20m -XX:+UseSerialGC -XX:+PrintGCDetails
+
+		// 查看GC信息
+		System.out.println("Max memory:" + Runtime.getRuntime().maxMemory());
+		System.out.println("Free Memory:" + Runtime.getRuntime().freeMemory());
+		System.out.println("Total memory:" + Runtime.getRuntime().totalMemory());
+
+		byte[] b1 = new byte[1 * 1024 * 1024];
+		System.out.println("分配了1M");
+		System.out.println("Max memory:" + Runtime.getRuntime().maxMemory());
+		System.out.println("Free Memory:" + Runtime.getRuntime().freeMemory());
+		System.out.println("Total memory:" + Runtime.getRuntime().totalMemory());
+
+		byte[] b2 = new byte[1 * 1024 * 1024];
+		System.out.println("分配了4M");
+		System.out.println("Max memory:" + Runtime.getRuntime().maxMemory());
+		System.out.println("Free Memory:" + Runtime.getRuntime().freeMemory());
+		System.out.println("Total memory:" + Runtime.getRuntime().totalMemory());
+
+	}
 
 }
