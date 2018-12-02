cbb31ce90f839b3eb0711fdc7c826ff08ffe86fd
wx91 <766221790@qq.com>
2018-12-02 17:49:38 +0800

    删除文件

--- SpringCloud&Docker/chapter-3/microservice-simple-provider-user/src/main/java/com/wangx/cloud/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wangx.cloud.repository;
-
-import com.wangx.cloud.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
