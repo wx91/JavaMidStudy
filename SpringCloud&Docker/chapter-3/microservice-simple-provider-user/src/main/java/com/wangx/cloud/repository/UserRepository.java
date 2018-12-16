ce886aaea85e5663b1f00ed8b1e2271c05e0bb2a
wx91 <766221790@qq.com>
2018-12-16 20:26:26 +0800

    重构项目结构

--- SpringCloud&Docker/chapter-3/microservice-simple-provider-user/src/main/java/com/wangx/cloud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wangx.cloud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wangx.cloud.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
